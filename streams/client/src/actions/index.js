8564045a537e8b6794aec6c8c6ab87b42e8b39ea
EricHan <hxjnewzealand@gmail.com>
2019-07-10 13:20:18 +1200

    Create post api in actions

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,3 +1,4 @@
+import streams from "../apis";
 import { SIGN_IN, SIGN_OUT } from "./types";
 
 export const signIn = userId => {
@@ -11,4 +12,9 @@ export const signOut = () => {
   return {
     type: SIGN_OUT
   };
+};
+
+// 新建API POST 方法， 传入表格内容并异步存到数据库
+export const createStream = formValues => async dispatch => {
+  streams.post("/streams", formValues);
 };
