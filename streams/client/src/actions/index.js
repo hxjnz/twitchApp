b462d11ecf216e0e26442e52daf88dac1dada86f
EricHan <hxjnewzealand@gmail.com>
2019-07-10 14:41:24 +1200

    Create all stream reducers for apis

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -28,3 +28,27 @@ export const createStream = formValues => async dispatch => {
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
+
+export const fetchStreams = formValues => async dispatch => {
+  const response = await streams.post("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  const response = await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
